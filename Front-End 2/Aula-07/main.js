641e491368c42784f828c23144e3d81703647a06
Loretto <eliveltonalves18@gmail.com>
2022-03-16 21:59:04 -0300

    Atualização 2 Semana Março

---
+++ Front-End 2/Aula-07/main.js
@@ -0,0 +1,11 @@
+let buttonReference = document.querySelector('#button')
+let titleHintReference = document.querySelector('#titleHint')
+let titleHintReference2 = document.querySelector('#titleHint2')
+let resultado = document.querySelector('#resultado')
+let titleInputReference = document.querySelector('#valor1')
+let titleInputReference2 = document.querySelector('#valor2')
+let mensagemSucessoReference = document.querySelector('#mensagemSucesso')
+
+function subtrair(operacao){
+    
+}
