ad72647f3b4f06db62bd2671bdbd236b96bc432a
Loretto <eliveltonalves18@gmail.com>
2022-03-21 01:37:49 -0300

    Atualizações

--- Front-End 2/Checkpoint_1_FontEnd2/main.js
+++ Front-End 2/Checkpoint_1_FontEnd2/main.js
@@ -1,5 +1,35 @@
-let bodyReference = document.querySelector('dark');
+//inputs
+let nomePersonagemReference = document.querySelector('#NomePersonagem')
+let habilidadeReference = document.querySelector('#habilidade')
+let descricaoReference= document.querySelector('#descricao')
+let imagemReference = document.querySelector('#imagem')
+//botões
+let resetReference = document.querySelector('#reset')
+let loginReference = document.querySelector('#login')
 
-function darkMode(){
-    bodyReference.classList.toogle('dark')
-}
+
+loginReference.addEventListener("click",function(event) {
+    event.preventDefault()
+    let card = {
+        nome: nomePersonagemReference.value,
+        habilidade: habilidadeReference.value,
+        descricao: descricaoReference.value,
+        imagem: imagemReference.value
+    }
+    let containerReference = document.querySelector('.container')
+    let cards = [
+        containerReference.innerHTML+= 
+            `<div>
+            <img src="${card.imagem}"></img>
+            <h3>${card.nome}</h3>
+            <h4>${card.nome}</h4>
+            <p> ${card.descricao} </p>
+            </div>`
+    ]
+    cards.push(card)
+    console.log(card)
+})
+
+
+
+
