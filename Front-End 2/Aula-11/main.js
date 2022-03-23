9705514cd4aee0df21d33c5faf09c3e0b5eb2c10
Loretto <eliveltonalves18@gmail.com>
2022-03-23 00:01:14 -0300

    Atualização Checkpoint 22-03

---
+++ Front-End 2/Aula-11/main.js
@@ -0,0 +1,12 @@
+let botaoLoginReferencia = document.querySelector('#botaoLogin')
+let inputEmailReferencia = document.querySelector('#botaoEmail')
+
+
+botaoLoginReferencia.addEventListener('click', function(event) {
+
+    event.preventDefault()
+
+    //console.log(event)
+
+
+})
